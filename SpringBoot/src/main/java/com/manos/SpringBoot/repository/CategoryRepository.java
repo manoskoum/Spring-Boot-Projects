cbcff803bfcd4c802449b314f15242fdbb9f20a0
Emmanouil Koumentakis <manoskoume@gmail.com>
2025-02-23 13:15:38 +0200

    Delete SpringBoot directory

--- SpringBoot/src/main/java/com/manos/SpringBoot/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manos.SpringBoot.repository;
-
-import com.manos.SpringBoot.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-    Category findByCategoryName(String categoryName);
-}
