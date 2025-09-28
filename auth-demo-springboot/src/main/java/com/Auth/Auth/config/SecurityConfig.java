004d0bc63986e700b25b04c6592ff8d21378cf5a
Emmanouil Koumentakis <manoskoume@gmail.com>
2025-09-28 14:57:33 +0300

    Add files via upload

---
+++ auth-demo-springboot/src/main/java/com/Auth/Auth/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.Auth.Auth.config;
+
+import com.Auth.Auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Autowired
+    private  UserRepository userRepository;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true) // όταν κάνει login σωστά
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository repo) {
+        return username -> userRepository.findByUsername(username)
+                .map(u -> User.withUsername(u.getUsername())
+                        .password(u.getPasswordHash())
+                        .roles("USER").build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+
+    }
+
